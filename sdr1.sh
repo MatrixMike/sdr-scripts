63d8b81292b46555871cab1132722e4bc32b62bb
Mike J H RaspberryPi <matroxmike@icloud.com>
2019-02-10 16:24:44 +1100

    split build

--- sdr1.sh
+++ sdr1.sh
@@ -13,8 +13,7 @@ apt-get install freeglut3-dev freeglut3
 # Now we'll git the projects we need
 
 git clone https://github.com/jgaeddert/liquid-dsp
-wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2
-tar -xvjf wxWidgets-3.1.0.tar.bz2 
+
 git clone https://github.com/pothosware/SoapySDR.git
 git clone https://github.com/pothosware/SoapySDRPlay.git
 git clone https://github.com/pothosware/SoapyRemote.git
@@ -31,14 +30,15 @@ ldconfig
 cd ..
 
 # Build wxwidgets
-
-cd wxWidgets-3.1.0/
-mkdir -p ~/Develop/wxWidgets-staticlib
-./autogen.sh
-./configure --with-opengl --disable-shared --enable-monolithic --with-libjpeg --with-libtiff --with-libpng --with-zlib --disable-sdltest --enable-unicode --enable-display --enable-propgrid --disable-webkit --disable-webview --disable-webviewwebkit --prefix=`echo ~/Develop/wxWidgets-staticlib` CXXFLAGS="-std=c++0x" --with-libiconv=/usr
-make -j4
-make install
-cd ..
+#wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2
+#tar -xvjf wxWidgets-3.1.0.tar.bz2 
+#cd wxWidgets-3.1.0/
+#mkdir -p ~/Develop/wxWidgets-staticlib
+#./autogen.sh
+#./configure --with-opengl --disable-shared --enable-monolithic --with-libjpeg --with-libtiff --with-libpng --with-zlib --disable-sdltest --enable-unicode --enable-display --enable-propgrid --disable-webkit --disable-webview --disable-webviewwebkit --prefix=`echo ~/Develop/wxWidgets-staticlib` CXXFLAGS="-std=c++0x" --with-libiconv=/usr
+#make -j4
+#make install
+#cd ..
 
 echo "."
 echo "First part of installation done.   Now run the SDRPlay file you"
