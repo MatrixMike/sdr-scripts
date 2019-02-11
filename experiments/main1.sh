e181c9ed9ee54e85eea1522e39ed17c57b0c8043
Mike <matroxmike@icloud.com>
2019-02-11 19:43:55 +1100

    experiments

---
+++ experiments/main1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#main shell
+touch mainfile1.txt 
+./subsh1.sh
+touch mainfile2.txt
+./subsh1.sh
+
---
+++ experiments/subsh1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#sub shell
+mkdir new1
+cd new1 || exit
+touch newfile1.txt 
+
