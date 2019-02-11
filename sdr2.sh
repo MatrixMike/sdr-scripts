aff6ebaa20ed76ecc37079489b5cb408e7efd46b
Mike <matroxmike@icloud.com>
2019-02-11 15:27:24 +1100

    rem out for test

--- sdr2.sh
+++ sdr2.sh
@@ -8,54 +8,56 @@
 
 #cd SoapySDR
 cd SoapySDR || exit
-mkdir build
+##*mkdir build
 #cd build
-cd build || exit
-cmake ..
-make -j4
-make install
-ldconfig
-cd ..
-cd ..
+##*cd build || exit
+##*cmake ..
+##*make -j4
+##*make install
+##*ldconfig
+##*cd ..
+##*cd ..
 
 # Now we build the SDRPlay module for Soapy
 
 cd SoapySDRPlay || exit
-mkdir build
-cd build || exit
-cmake ..
-make
-make install
-cd ..
-cd ..
+./sub1.sh
+#mkdir build
+#cd build || exit
+#cmake ..
+#make
+#make install
+#cd ..
+#cd ..
 
 # And we build SoapyRemote
 
 cd SoapyRemote || exit
-mkdir build
-cd build || exit
-cmake ..
-make
-make install
-cd ..
-cd ..
+./sub1.sh
+#mkdir build
+#cd build || exit
+#cmake ..
+#make
+#make install
+#cd ..
+#cd ..
 
 # And finally,  we build Cubic.  This takes a while!
 
-cd CubicSDR || exit
-mkdir build
-cd build || exit
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DwxWidgets_CONFIG_EXECUTABLE=~/Develop/wxWidgets-staticlib/bin/wx-config
-make
-make install
-cd ..
-cd ..
-rm -R ~/Develop
+##*cd CubicSDR || exit
+##*mkdir build
+##*cd build || exit
+##*cmake ../ -DCMAKE_BUILD_TYPE=Release -DwxWidgets_CONFIG_EXECUTABLE=~/Develop/wxWidgets-staticlib/bin/wx-config
+##*make
+##*make install
+##*cd ..
+##*cd ..
+##*rm -R ~/Develop
 
 # now we change permissions on these root-owned folders so the user can
 # delete them at their leisure.
 
-chmod -R 0777 ./*
+##*chmod -R 0777 ./*
 
 # And we're done.   Cubic should work with the SDRPlay.  The user can
 # run it from the terminal with CubicSDR or add this command to a menu.
